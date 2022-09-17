71e3f13f1e9db30e35ee64f0589848ca1e09fc60
miguelsb2210 <100372495+miguelsb2210@users.noreply.github.com>
2022-09-17 11:31:19 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,59 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+
+let engine;
+let world;
+var ball;
+var ground;
+var wedge;
+var angle=60;
+var poly;
+
+
+function setup() {
+  
+  createCanvas(400,400);
+  engine = Engine.create();
+  world = engine.world;
+
+  var ball_options = {
+    restitution : 0.95,
+    frictionAir : 0.01
+  }
+
+  var ground_options = {
+    isStatic : true
+  }
+
+  var quadrado={
+    restitution : 0.95,
+    frictionAir : 0.05
+  }
+
+  ball = Bodies.circle(100,10,20,ball_options);
+  World.add(world,ball);
+
+  ground = Bodies.rectangle(200,390,400,20,ground_options);
+  World.add(world,ground);
+
+  quadrado2 = Bodies.rectangle(100,10,20,20,quadrado);
+  World.add(world,quadrado2);
+
+  rectMode(CENTER);
+  ellipseMode(RADIUS);
+}
+
+
+function draw() 
+{
+ background (51);
+ Engine.update(engine);
+
+ ellipse(ball.position.x,ball.position.y,20);
+ rect(ground.position.x,ground.position.y,400,20);
+ rect(quadrado2.position.x,quadrado2.position.y,20,20);
+}
+
